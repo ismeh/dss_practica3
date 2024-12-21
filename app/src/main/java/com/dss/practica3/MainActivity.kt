3465b152ded9f19fc130e98b6d0fcf3becd99772
ismeh <gadeismael@hotmail.com>
2024-12-21 08:19:34 +0100

    update: map current progress

--- app/src/main/java/com/dss/practica3/MainActivity.kt
+++ app/src/main/java/com/dss/practica3/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.dss.practica3
 
+import android.content.pm.PackageManager
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -33,4 +36,6 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
+
+
 }
--- app/src/main/java/com/dss/practica3/ui/googlemap/MapsActivity.kt
+++ app/src/main/java/com/dss/practica3/ui/googlemap/MapsActivity.kt
@@ -1,27 +1,44 @@
 package com.dss.practica3.ui.googlemap
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
 import com.dss.practica3.databinding.ActivityMapsBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 
 
 // FragmentActivity o AppCompatActivity o Fragment()?
 //class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+class MapsActivity : FragmentActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient // Google Play Services Location Provider
+    private lateinit var locationRequest: LocationRequest
+    private var currentLocationMarker: Marker? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,6 +50,19 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
             .findFragmentById(com.dss.practica3.R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        // Comprobar permisos de ubicación
+//        checkLocationPermission()
+
+//        // Inicializar FusedLocationProviderClient
+//        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+//
+//        // Configurar las actualizaciones de ubicación
+//         LocationRequest.Builder(locationRequest)
+//             .setIntervalMillis(5000)
+//             .setPriority(Priority.PRIORITY_HIGH_ACCURACY)
+//             .setMinUpdateIntervalMillis(2000)
+//             .build()
 
     }
 
@@ -68,9 +98,100 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        // Add a marker in Maracena and move the camera
+        val maracena = LatLng(37.20845336833197, -3.6315063638540823)
+        mMap.addMarker(MarkerOptions().position(maracena).title("Marcador en el Kebab Maracena"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(maracena))
+
+//        // Habilitar el botón de ubicación en el mapa
+//        if (ActivityCompat.checkSelfPermission(
+//                this,
+//                Manifest.permission.ACCESS_FINE_LOCATION
+//            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+//                this,
+//                Manifest.permission.ACCESS_COARSE_LOCATION
+//            ) != PackageManager.PERMISSION_GRANTED
+//        ) {
+//            // TODO: Consider calling
+//            //    ActivityCompat#requestPermissions
+//            // here to request the missing permissions, and then overriding
+//            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+//            //                                          int[] grantResults)
+//            // to handle the case where the user grants the permission. See the documentation
+//            // for ActivityCompat#requestPermissions for more details.
+//            return checkLocationPermission()
+//        }
+//        mMap.isMyLocationEnabled = true
+//
+//        // Comenzar a recibir actualizaciones de ubicación
+//        startLocationUpdates()
+    }
+
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            startLocationUpdates()
+        } else {
+            // Maneja el caso donde el permiso es rechazado
+        }
+    }
+
+    private fun checkLocationPermission() {
+        when {
+            ContextCompat.checkSelfPermission(
+                this, android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                // El permiso ya ha sido otorgado
+                startLocationUpdates()
+            }
+            else -> {
+                // Solicitar permiso
+                requestPermissionLauncher.launch(android.Manifest.permission.ACCESS_FINE_LOCATION)
+            }
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun startLocationUpdates() {
+        fusedLocationProviderClient.requestLocationUpdates(
+            locationRequest,
+            locationCallback,
+            mainLooper
+        )
+    }
+
+    private val locationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            super.onLocationResult(locationResult)
+
+            // Obtener la última ubicación
+            val location = locationResult.lastLocation
+            val currentLatLng = location?.let { LatLng(it.latitude, location.longitude) }
+
+            if (currentLatLng == null) {
+                return
+            }
+
+            // Actualizar el marcador en el mapa
+            if (currentLocationMarker == null) {
+                // Agregar un marcador nuevo si no existe
+                currentLocationMarker = mMap.addMarker(
+                    MarkerOptions().position(currentLatLng).title("Ubicación actual")
+                )
+            } else {
+                // Mover el marcador existente
+                currentLocationMarker?.position = currentLatLng
+            }
+
+            // Mover la cámara al marcador
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 15f))
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        // Detener las actualizaciones de ubicación al cerrar la actividad
+        fusedLocationProviderClient.removeLocationUpdates(locationCallback)
     }
 }
